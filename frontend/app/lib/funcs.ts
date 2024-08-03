50b0401f5e5d1f9e51d7ade87e741d94f75ceb63
spuckhafte <rakshit240405@gmail.com>
2024-08-03 23:32:11 +0530

    chore: clean unused functions

--- frontend/app/lib/funcs.ts
+++ frontend/app/lib/funcs.ts
@@ -1,12 +1,5 @@
 import { ZodError } from "zod";
 
-export async function getIP() {
-    return await runAsyncFuncUntilValidResponse(async () => {
-        const ip = await fetch("https://ipapi.co/json");
-        return (await ip.json()).ip;
-    }) as string;
-}
-
 export function getServerAddr() {
     return "http://localhost:5000";
 }
@@ -18,11 +11,4 @@ export function LsKey(key: string) {
 export function getZodError(e: any): string {
     const err = e as ZodError;
     return JSON.parse(err.message)[0].message;
-}
-
-async function runAsyncFuncUntilValidResponse(func: () => Promise<any>) {
-    const val = await func();
-    if (!val)
-        return runAsyncFuncUntilValidResponse(func);
-    else return val;
 }
